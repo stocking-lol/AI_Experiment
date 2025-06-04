ce51862e1a6289cebdad28bdfad1cabcab061894
stocking_lol <1697507659@qq.com>
2025-06-04 23:01:41 +0800

    push

--- Project_6_Machine_Learning_full/models.py
+++ Project_6_Machine_Learning_full/models.py
@@ -1,6 +1,7 @@
 import nn
+import numpy as np
 
-### Question 1
+# Question 1
 class PerceptronModel(object):
     def __init__(self, dimensions):
         """
@@ -28,7 +29,7 @@ class PerceptronModel(object):
         Returns: a node containing a single number (the score)
         """
         "*** YOUR CODE HERE ***"
-
+        return nn.DotProduct(x, self.get_weights())
 
     def get_prediction(self, x):
         """
@@ -37,7 +38,8 @@ class PerceptronModel(object):
         Returns: 1 or -1
         """
         "*** YOUR CODE HERE ***"
-
+        score = nn.as_scalar(self.run(x))
+        return 1 if score >= 0 else -1
 
     def train(self, dataset):
         """
@@ -47,18 +49,40 @@ class PerceptronModel(object):
         retrieve seft.data in nn.Constant, e.g., if x is an object of nn.Constant, get its data via x.data
         """
         "*** YOUR CODE HERE ***"
+        converged = False
+        while not converged:
+            converged = True
+            for x, y in dataset.iterate_once(1):
+                true_label = nn.as_scalar(y)
+                pre_label = self.get_prediction(x)
+                if pre_label != true_label:
+                    converged = False
+                    direction = x if true_label == 1 else nn.Constant(-x.data)
+                    self.w.update(direction, 1.0)
 
 
-### Question 2
+# Question 2
 class RegressionModel(object):
     """
     A neural network model for approximating a function that maps from real
     numbers to real numbers. The network should be sufficiently large to be able
     to approximate sin(x) on the interval [-2pi, 2pi] to reasonable precision.
     """
+
     def __init__(self):
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        self.batchsize = 50
+        self.learning_rate = 0.01
+        self.hidden_size = 200
+
+        self.w1 = nn.Parameter(1, self.hidden_size)
+        self.w2 = nn.Parameter(self.hidden_size, self.hidden_size)
+        self.w3 = nn.Parameter(self.hidden_size, 1)
+        self.b1 = nn.Parameter(1, self.hidden_size)
+        self.b2 = nn.Parameter(1, self.hidden_size)
+        self.b3 = nn.Parameter(1, 1)
+
 
     def run(self, x):
         """
@@ -70,6 +94,10 @@ class RegressionModel(object):
             A node with shape (batch_size x 1) containing predicted y-values
         """
         "*** YOUR CODE HERE ***"
+        hidden1 = nn.ReLU(nn.AddBias(nn.Linear(x, self.w1), self.b1))
+        hidden2 = nn.ReLU(nn.AddBias(nn.Linear(hidden1, self.w2), self.b2))
+        output = nn.AddBias(nn.Linear(hidden2, self.w3), self.b3)
+        return output
 
     def get_loss(self, x, y):
         """
@@ -82,15 +110,26 @@ class RegressionModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
-
+        return nn.SquareLoss(self.run(x), y)
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        for x, y in dataset.iterate_forever(self.batchsize):
+            loss = self.get_loss(x, y)
+            gradiant = nn.gradients(loss, [self.w1, self.b1, self.w2, self.b2])
 
-### Question 3
+            # 更新梯度
+            for param, grad in zip([self.w1, self.b1, self.w2, self.b2], gradiant):
+                param.update(grad, -self.learning_rate)
+
+            if nn.as_scalar(loss) < 0.01:
+                return
+
+
+# Question 3
 class DigitClassificationModel(object):
     """
     A model for handwritten digit classification using the MNIST dataset.
@@ -105,9 +144,21 @@ class DigitClassificationModel(object):
     methods here. We recommend that you implement the RegressionModel before
     working on this part of the project.)
     """
+
     def __init__(self):
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        self.batchsize = 50
+        self.learning_rate = 0.05
+        self.hidden_size = 200
+
+        self.w1 = nn.Parameter(784, self.hidden_size)
+        self.w2 = nn.Parameter(self.hidden_size, self.hidden_size)
+        self.w3 = nn.Parameter(self.hidden_size, 10)
+        self.b1 = nn.Parameter(1, self.hidden_size)
+        self.b2 = nn.Parameter(1, self.hidden_size)
+        self.b3 = nn.Parameter(1, 10)
+
 
     def run(self, x):
         """
@@ -124,7 +175,10 @@ class DigitClassificationModel(object):
                 (also called logits)
         """
         "*** YOUR CODE HERE ***"
-
+        hidden1 = nn.ReLU(nn.AddBias(nn.Linear(x, self.w1), self.b1))
+        hidden2 = nn.ReLU(nn.AddBias(nn.Linear(hidden1, self.w2), self.b2))
+        output = nn.AddBias(nn.Linear(hidden2, self.w3), self.b3)
+        return output
 
     def get_loss(self, x, y):
         """
@@ -140,17 +194,25 @@ class DigitClassificationModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
-
-
+        return nn.SoftmaxLoss(self.run(x), y)
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        for i, (x, y) in enumerate(dataset.iterate_forever(self.batchsize)):
+            loss = self.get_loss(x, y)
+            gradients = nn.gradients(loss, [self.w1, self.b1, self.w2, self.b2, self.w3, self.b3])
 
+            # Update parameters
+            for param, grad in zip([self.w1, self.b1, self.w2, self.b2, self.w3, self.b3], gradients):
+                param.update(grad, -self.learning_rate)
 
+            # Stop condition
+            if i % 100 == 0 and dataset.get_validation_accuracy() > 0.975:
+                return
 
-### Question 4
+# Question 4
 class LanguageIDModel(object):
     """
     A model for language identification at a single-word granularity.
@@ -158,6 +220,7 @@ class LanguageIDModel(object):
     methods here. We recommend that you implement the RegressionModel before
     working on this part of the project.)
     """
+
     def __init__(self):
         # Our dataset contains words from five different languages, and the
         # combined alphabets of the five languages contain a total of 47 unique
@@ -168,7 +231,18 @@ class LanguageIDModel(object):
 
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        self.num_chars = 47
+        self.languages = ["English", "Spanish", "Finnish", "Dutch", "Polish"]
+        self.batch_size = 100
+        self.learning_rate = 0.1
+        self.hidden_size = 100
 
+        # Initialize parameters
+        self.w_xh = nn.Parameter(self.num_chars, self.hidden_size)
+        self.w_hh = nn.Parameter(self.hidden_size, self.hidden_size)
+        self.b_h = nn.Parameter(1, self.hidden_size)
+        self.w_hy = nn.Parameter(self.hidden_size, 5)
+        self.b_y = nn.Parameter(1, 5)
     def run(self, xs):
         """
         Runs the model for a batch of examples.
@@ -195,7 +269,14 @@ class LanguageIDModel(object):
                 (also called logits)
         """
         "*** YOUR CODE HERE ***"
+        h = nn.Constant(np.zeros((xs[0].data.shape[0], self.hidden_size)))
+        for x in xs:
+            h = nn.ReLU(nn.AddBias(
+                nn.Add(nn.Linear(x, self.w_xh),
+                       nn.Linear(h, self.w_hh)
+                       ), self.b_h))
 
+        return nn.AddBias(nn.Linear(h, self.w_hy), self.b_y)
     def get_loss(self, xs, y):
         """
         Computes the loss for a batch of examples.
@@ -209,9 +290,23 @@ class LanguageIDModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
+        return nn.SoftmaxLoss(self.run(xs), y)
+
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        for i, (xs, y) in enumerate(dataset.iterate_forever(self.batch_size)):
+            loss = self.get_loss(xs, y)
+            params = [self.w_xh, self.w_hh, self.b_h, self.w_hy, self.b_y]
+            gradients = nn.gradients(loss, params)
+
+            # Update parameters
+            for param, grad in zip(params, gradients):
+                param.update(grad, -self.learning_rate)
+
+            # Stop condition
+            if i % 100 == 0 and dataset.get_validation_accuracy() > 0.86:
+                return
